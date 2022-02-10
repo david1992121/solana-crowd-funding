1e915c52e47281b5bcfddfb48d0e27dab44470c9
jackson1992121 <dantes0121@hotmail.com>
2022-02-10 11:12:32 +0800

    add campaign functions, structs

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,8 +1,127 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+// First we include what we are going to need in our program. 
+// This  is the Rust style of importing things.
+// Remember we added the dependencies in cargo.toml
+// And from the `solana_program` crate we are including  all the required things.
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    rent::Rent,
+    sysvar::Sysvar,
+};
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+
+// Every solana program has one entry point
+// And it is a convention to name it `process_instruction`. 
+// It should take in program_id, accounts, instruction_data as parameters.
+fn process_instruction(
+    // program id is nothing but the id of this program on the solana network.
+    program_id: &Pubkey,
+    // When we invoke our program we can 
+    // give meta data of all the account we 
+    // want to work with.
+    // As you can see it is a array of AccountInfo.
+    // We can provide as many as we want.
+    accounts: &[AccountInfo],
+    // This is the data we want to process our instruction for.
+    // It is a list of 8 bitunsigned integers(0..255).
+    instruction_data: &[u8],
+    
+    // Here we specify the return type.
+    // If you know a little bit of typescript. 
+    // This was of writing types and returns types might we familiar to you.
+) -> ProgramResult {
+    if instruction_data.len() == 0 {
+        return Err(ProgramError::InvalidInstructionData);
     }
+
+    // Now we just check and call the function for each of them.
+    // I have choosen 0 for create_campaign,
+    // 1 for withdraw
+    // 2 for donate.
+    if instruction_data[0] == 0 {
+        return create_campaign(
+            program_id,
+            accounts,
+            // Notice we pass program_id and accounts as they where 
+            // but we pass a reference to slice of [instruction_data]. 
+            // we do not want the first element in any of our functions.
+            &instruction_data[1..instruction_data.len()],
+        );
+    } else if instruction_data[0] == 1 {
+        return withdraw(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    } else if instruction_data[0] == 2 {
+        return donate(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    }
+
+    // If instruction_data doesn't match we give an error.
+    // Note I have used msg!() macro and passed a string here. 
+    // It is good to do this as this would 
+    // also get printed in the console window
+    // if a program fails.
+    msg!("Didn't find the entrypoint required");
+    Err(ProgramError::InvalidInstructionData)
+}
+
+
+// Then we call the entry point macro to add `process_instruction` as our entry point to our program.
+entrypoint!(process_instruction);
+
+fn create_campaign(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    Ok(())
+}
+
+fn withdraw(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    Ok(())
+}
+
+fn donate(
+    program_id: &Pubkey, 
+    accounts: &[AccountInfo], 
+    _instruction_data: &[u8]
+) -> ProgramResult {
+    Ok(())
+}
+
+/// For an example, let us create human struct.
+#[derive(Debug)]
+struct Human {
+    /// we can add all the fields in our struct here.
+    /// we also have to specify the type of each variable.
+    /// Like the [eyes_color] here is a `String` type object.
+    pub eyes_color: String,
+    pub name: String,
+    pub height: i32,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct CampaignDetails {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub image_link: String,
+    /// we will be using this to know the total amount 
+    /// donated to a campaign.
+    pub amount_donated: u64,
 }
